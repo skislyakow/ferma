1f0b46a5c25b329338955f52e75ccd2ccdfd3b88
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-06-06 09:25:15 +0300

    Switch fallback image to repost2.png

--- core/lightning/run_lightning.py
+++ core/lightning/run_lightning.py
@@ -35,7 +35,7 @@ BREAKING_KEYWORDS = [
 ]
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-REPOST_BANNER = os.path.join(PROJECT_ROOT, "repost.png")
+REPOST_BANNER = os.path.join(PROJECT_ROOT, "repost2.png")
 SESSION_FILE = "repost.session"
 
 
