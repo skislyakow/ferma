ac65714642697116287d545f91866000a819371b
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-07-12 21:04:52 +0300

    fix published.json path for interesting: move to channels/ dir, atomic write, fix analytics path

--- core/analytics.py
+++ core/analytics.py
@@ -140,7 +140,7 @@ class FarmAnalytics:
 
             published_path = os.path.join(
                 os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-                "core",
+                "channels",
                 name,
                 "published.json",
             )
